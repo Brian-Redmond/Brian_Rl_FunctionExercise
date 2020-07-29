70e8fc92af6005e27dca0fb1fbf99ba9b26fc259
Brian-Redmond <67969029+Brian-Redmond@users.noreply.github.com>
2020-07-29 16:21:45 -0500

    Add files via upload

---
+++ Anti-Code/Anti-Code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anti-Code
+//
+//  Created by Brian Redmond on 7/29/20.
+//  Copyright © 2020 Brian Redmond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Anti-Code/Anti-CodeTests/Anti_CodeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Anti_CodeTests.swift
+//  Anti-CodeTests
+//
+//  Created by Brian Redmond on 7/29/20.
+//  Copyright © 2020 Brian Redmond. All rights reserved.
+//
+
+import XCTest
+@testable import Anti_Code
+
+class Anti_CodeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
